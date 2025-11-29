30331f6ed5c08894bf33cc05b49704e7580d5df8
andresenaia-bit <andresenaia@gmail.com>
2025-11-29 18:29:56 -0300

    Fix console log syntax error in server start message

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.get('/bem-vindo', (req, res) => {
 
 // Inicia o servidor
 app.listen(port\'' () => {
-  console.log(Servidor rodando na porta ${port});
+  console.log(Servidor rodando na porta ${port}'.
 });
